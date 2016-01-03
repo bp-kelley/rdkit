44c8e4a4b6ae77f9e8c7067b5f72abf243eba1f0
Brian Kelley <brian.kelley@novartis.com>
2016-01-03 10:36:58 -0500

    RDTag thread local store implementation

---
+++ Code/RDGeneral/tags.h
@@ -0,0 +1,82 @@
+#ifndef RDKIT_TAGS_H
+#define RDKIT_TAGS_H
+
+#include "BoostStartInclude.h"
+
+#ifdef RDK_TEST_MULTITHREADED
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/tss.hpp>
+#endif
+
+#include <boost/unordered_map.hpp>
+#include "BoostEndInclude.h"
+ 
+namespace RDKit
+{
+
+class RDTags
+{
+  typedef boost::unordered_map<std::string, int> MAP;
+public:
+  RDTags() {};
+
+  const std::string &get(int tag) const {
+    return keys[tag]; // raises exception
+  }
+  
+  int get(const std::string &k) const
+  {
+#ifdef RDK_TEST_MULTITHREADED
+    static boost::thread_specific_ptr<MAP> instance;
+
+    if ( !instance.get() ) {
+      boost::unique_lock<boost::mutex> lock(_m);
+      // enable TLS loaded with current map
+      instance.reset(new MAP(m));
+    }
+    
+    MAP &ptr = *instance;
+    // check tls map
+    {
+      MAP::const_iterator it=ptr.find(k);
+      if (it != ptr.end())
+        return it->second;
+    }
+#endif
+    
+    {
+      boost::unique_lock<boost::mutex> lock(_m);
+      
+      {
+        MAP::const_iterator it=m.find(k);
+        
+        if (it != m.end()) {
+#ifdef RDK_TEST_MULTITHREADED          
+          // insert into local map        
+          instance->insert(*it);
+#endif
+          return it->second;
+        }
+      }
+      {
+        int res = m.size() + 1;
+        std::pair<std::string, int> p(k,res);
+#ifdef RDK_TEST_MULTITHREADED        
+        instance->insert(p);
+#endif        
+        m.insert(p);
+        keys.push_back(k);
+        return res;
+      }
+    }
+  }
+  
+private:
+    mutable boost::mutex _m;
+    mutable MAP m;
+    mutable std::vector<std::string > keys;
+};
+
+}
+
+#endif
