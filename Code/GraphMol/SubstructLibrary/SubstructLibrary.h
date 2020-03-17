26f652a408f725ef4fcdcdc8b6d1787d27e34f28
Brian Kelley <bkelley@relaytx.com>
2020-03-17 14:34:45 -0400

    Add boost threading for shared_mutex

--- Code/GraphMol/SubstructLibrary/SubstructLibrary.h
+++ Code/GraphMol/SubstructLibrary/SubstructLibrary.h
@@ -53,11 +53,11 @@ typedef std::mutex READWRITE_LOCK;
 # else
 #include <boost/thread.hpp>
 #include <boost/thread/shared_mutex.hpp>
-typedef boost::shared_mutex READWRITE_lock;
-# define READERLOCK(lock) boost::upgrade_lock< boost::shared_mutex > lock(lock)
+typedef boost::shared_mutex READWRITE_LOCK;
+# define READERLOCK(lock) boost::upgrade_lock< boost::shared_mutex > readerLock(lock)
 # define WRITERLOCK(lock) \
-    boost::upgrade_lock< boost::shared_mutex > lock(lock);\
-    boost::upgrade_to_unique_lock< boost::shared_mutex > uniqueLock(lock)
+    boost::upgrade_lock< boost::shared_mutex > readerLock(lock); \
+    boost::upgrade_to_unique_lock< boost::shared_mutex > uniqueLock(readerLock)
 #endif
 namespace RDKit {
 
